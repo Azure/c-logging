6636852a761b07fd09f76ebd85c6e64589ad020f
Dan Cristoloveanu <dcristo@microsoft.com>
2022-09-02 22:38:06 -0700

    Remove some include

--- v2/src/log_internal_error.c
+++ v2/src/log_internal_error.c
@@ -1,7 +1,9 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+#if defined _MSC_VER
 #include <stdlib.h>
+#endif
 
 #include "c_logging/log_internal_error.h"
 
