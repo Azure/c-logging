678d2b91e2fd837b283b18edc8b8f9bca13b6c1a
Dan Cristoloveanu <dcristo@microsoft.com>
2022-09-02 21:54:15 -0700

    Add tests and code for log_internal_error_report

--- v2/devdoc/log_internal_error_requirements.md
+++ v2/devdoc/log_internal_error_requirements.md
@@ -18,6 +18,8 @@ void log_internal_error_report(void);
 
 `log_internal_error_report` indicates to the user that a critical logging framework error has happened.
 
-**SRS_LOG_INTERNAL_ERROR_01_001: [** If `LOG_ABORT_ON_ERROR` is defined, `log_internal_error_report` shall `abort` the execution. **]**
+**SRS_LOG_INTERNAL_ERROR_01_003: [** On `Windows`, if `LOG_ABORT_ON_ERROR` is defined, `log_internal_error_report` shall call `_set_abort_behavior(_CALL_REPORTFAULT, _WRITE_ABORT_MSG | _CALL_REPORTFAULT)` to disable the abort message. **]**
+
+**SRS_LOG_INTERNAL_ERROR_01_001: [** If `LOG_ABORT_ON_ERROR` is defined, `log_internal_error_report` shall call `abort` the execution. **]**
 
 **SRS_LOG_INTERNAL_ERROR_01_002: [** Otherwise `log_internal_error_report` shall return. **]**
---
+++ v2/tests/log_internal_error_with_abort_ut/log_internal_error_with_abort_ut.c
@@ -0,0 +1,134 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "macro_utils/macro_utils.h"
+#include "c_logging/log_internal_error.h"
+
+static size_t asserts_failed = 0;
+
+// defines how many mock calls we can have
+#define MAX_MOCK_CALL_COUNT (128)
+
+#define MOCK_CALL_TYPE_VALUES \
+    MOCK_CALL_TYPE_abort, \
+    MOCK_CALL_TYPE__set_abort_behavior \
+
+MU_DEFINE_ENUM(MOCK_CALL_TYPE, MOCK_CALL_TYPE_VALUES)
+
+// very poor mans mocks :-(
+typedef struct _set_abort_behavior_CALL_TAG
+{
+    unsigned int _Flags;
+    unsigned int _Mask;
+} _set_abort_behavior_CALL;
+
+typedef struct MOCK_CALL_TAG
+{
+    MOCK_CALL_TYPE mock_call_type;
+    union
+    {
+        _set_abort_behavior_CALL _set_abort_behavior_call;
+    } u;
+} MOCK_CALL;
+
+static MOCK_CALL expected_calls[MAX_MOCK_CALL_COUNT];
+static size_t expected_call_count;
+static size_t actual_call_count;
+static bool actual_and_expected_match;
+
+static void setup_mocks(void)
+{
+    expected_call_count = 0;
+    actual_call_count = 0;
+    actual_and_expected_match = true;
+}
+
+void mock_abort(void)
+{
+    if ((actual_call_count == expected_call_count) ||
+        (expected_calls[actual_call_count].mock_call_type != MOCK_CALL_TYPE_abort))
+    {
+        actual_and_expected_match = false;
+    }
+    else
+    {
+        actual_call_count++;
+    }
+}
+
+unsigned int mock__set_abort_behavior(unsigned int _Flags, unsigned int _Mask)
+{
+    unsigned int result;
+
+    if ((actual_call_count == expected_call_count) ||
+        (expected_calls[actual_call_count].mock_call_type != MOCK_CALL_TYPE__set_abort_behavior) ||
+        (expected_calls[actual_call_count].u._set_abort_behavior_call._Flags != _Flags) ||
+        (expected_calls[actual_call_count].u._set_abort_behavior_call._Mask != _Mask))
+    {
+        actual_and_expected_match = false;
+        result = 0;
+    }
+    else
+    {
+        result = _set_abort_behavior(_Flags, _Mask);
+
+        actual_call_count++;
+    }
+
+    return result;
+}
+
+static void setup_abort(void)
+{
+    expected_calls[expected_call_count].mock_call_type = MOCK_CALL_TYPE_abort;
+    expected_call_count++;
+}
+
+static void setup__set_abort_behavior(unsigned int _Flags, unsigned int _Mask)
+{
+    expected_calls[expected_call_count].mock_call_type = MOCK_CALL_TYPE__set_abort_behavior;
+    expected_calls[expected_call_count].u._set_abort_behavior_call._Flags = _Flags;
+    expected_calls[expected_call_count].u._set_abort_behavior_call._Mask = _Mask;
+    expected_call_count++;
+}
+
+#define POOR_MANS_ASSERT(cond) \
+    if (!(cond)) \
+    { \
+        (void)printf("%s:%d test failed\r\n", __FUNCTION__, __LINE__); \
+        asserts_failed++; \
+    } \
+
+/* LOG_CONTEXT_CREATE */
+
+/* Tests_SRS_LOG_INTERNAL_ERROR_01_001: [ If LOG_ABORT_ON_ERROR is defined, log_internal_error_report shall call abort the execution. ]*/
+/* Tests_SRS_LOG_INTERNAL_ERROR_01_003: [ On Windows, if LOG_ABORT_ON_ERROR is defined, log_internal_error_report shall call _set_abort_behavior(_CALL_REPORTFAULT, _WRITE_ABORT_MSG | _CALL_REPORTFAULT) to disable the abort message. ]*/
+static void log_internal_error_report_aborts(void)
+{
+    // arrange
+    setup_mocks();
+#if defined _MSC_VER
+    setup__set_abort_behavior(_CALL_REPORTFAULT, _WRITE_ABORT_MSG | _CALL_REPORTFAULT);
+#endif
+    setup_abort();
+
+    // act
+    log_internal_error_report();
+
+    // assert
+    POOR_MANS_ASSERT(actual_and_expected_match);
+    POOR_MANS_ASSERT(expected_call_count == actual_call_count);
+}
+
+/* very "poor man's" way of testing, as no test harness and mocking framework are available */
+int main(void)
+{
+    log_internal_error_report_aborts();
+
+    return asserts_failed;
+}
