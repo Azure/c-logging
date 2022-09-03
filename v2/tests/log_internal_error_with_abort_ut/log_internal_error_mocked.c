10fa1161abba14d0980031ab5e7b6dbda606aaac
Dan Cristoloveanu <dcristo@microsoft.com>
2022-09-02 22:47:21 -0700

    Add an IWYU pragma keep

--- v2/tests/log_internal_error_with_abort_ut/log_internal_error_mocked.c
+++ v2/tests/log_internal_error_with_abort_ut/log_internal_error_mocked.c
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#include <stdlib.h>
+#include <stdlib.h> // IWYU pragma: keep
 
 #define abort mock_abort
 #define _set_abort_behavior mock__set_abort_behavior
