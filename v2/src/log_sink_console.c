04487bc0052a1137793b5f1d9ed296ef749198b6
Dan Cristoloveanu <dcristo@microsoft.com>
2022-10-13 10:46:35 -0700

    Make IWYU happy

--- v2/src/log_sink_console.c
+++ v2/src/log_sink_console.c
@@ -1,10 +1,9 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+#include "c_logging/log_level.h"
 #include "c_logging/log_context.h"
 #include "c_logging/log_sink_if.h"
-
-#include "c_logging/log_sink_console.h"
 
 static void log_sink_console_log(LOG_LEVEL log_level, LOG_CONTEXT_HANDLE log_context, const char* file, const char* func, int line, const char* message_format, ...)
 {
