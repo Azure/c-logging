2fddeccd372cceefb0759e1b258654775bcfd612
Dan Cristoloveanu <dcristo@microsoft.com>
2023-05-19 23:37:48 -0700

    Fix IWYU

--- v2/tests/log_sink_console_int/log_sink_console_int.c
+++ v2/tests/log_sink_console_int/log_sink_console_int.c
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#include <stdio.h>
+#include <stdio.h> // IWYU pragma: keep
 #include <stdint.h>
 #include <stdlib.h> // IWYU pragma: keep
 
