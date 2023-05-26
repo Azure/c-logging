53163d0f2e73b3a437a91837c68c62c520d4673e
Dan Cristoloveanu <dcristo@microsoft.com>
2023-05-26 15:50:23 -0700

    Add options so that we can control sinks independently

--- v2/src/logger_sinks_config.c
+++ v2/src/logger_sinks_config.c
@@ -7,13 +7,21 @@
 #include "c_logging/log_sink_console.h"
 #include "c_logging/log_sink_etw.h"
 
-const LOG_SINK_IF* log_sinks[] = 
-{
-#ifdef USE_TRACELOGGING
-    &log_sink_console
-#endif // USE_TRACELOGGING
-#ifdef CALL_CONSOLE_LOGGER
-    , &log_sink_etw
-}; // CALL_CONSOLE_LOGGER
-
+#if USE_LOG_SINK_CONSOLE || USE_LOG_SINK_ETW
+const LOG_SINK_IF* log_sinks[] = {
+#ifdef USE_LOG_SINK_CONSOLE
+    & log_sink_console,
+#endif // USE_LOG_SINK_CONSOLE
+#ifdef USE_LOG_SINK_ETW
+    & log_sink_etw
+#endif // USE_LOG_SINK_ETW
+};
 const uint32_t log_sink_count = MU_COUNT_ARRAY_ITEMS(log_sinks);
+
+#else // USE_LOG_SINK_CONSOLE || USE_LOG_SINK_ETW
+
+const LOG_SINK_IF* log_sinks[];
+const uint32_t log_sink_count = 0;
+
+#endif // USE_LOG_SINK_CONSOLE || USE_LOG_SINK_ETW
+
