49373dd1a3d8405b0b7944ca049221f1fcc89872
Dan Cristoloveanu <dcristo@microsoft.com>
2023-05-31 22:45:40 -0700

    Add perf test for logging v2 (#130)
    * Add skeleton for perf test
    
    * Add perf tests
    
    * Update main of perf tests
    
    * Copyright header minor fix
    
    * Fix int tests
    
    * Add perf tests to gate

---
+++ v2/tests/logger_perf/timer.h
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifndef TIMER_H
+#define TIMER_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+// This is a minimal version of the timer unit (just for tests)
+
+double timer_global_get_elapsed_us(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TIMER_H */
---
+++ v2/tests/logger_perf/timer_win32.c
@@ -0,0 +1,29 @@
+// Copyright (C) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include <stdio.h>
+
+#include "windows.h"
+
+#include "macro_utils/macro_utils.h"
+
+#include "timer.h"
+
+// This is a minimal version of the timer unit (just for tests)
+
+static LARGE_INTEGER g_freq;
+static volatile LONG g_timer_state = 0; /*0 - not "created", 1 - "created", "2" - creating*/
+
+/*returns a time in us since "some" start.*/
+double timer_global_get_elapsed_us(void)
+{
+    while (InterlockedCompareExchange(&g_timer_state, 2, 0) != 1)
+    {
+        (void)QueryPerformanceFrequency(&g_freq); /*from MSDN:  On systems that run Windows XP or later, the function will always succeed and will thus never return zero.*/
+        (void)InterlockedExchange(&g_timer_state, 1);
+    }
+
+    LARGE_INTEGER now;
+    (void)QueryPerformanceCounter(&now);
+    return (double)now.QuadPart * 1000000.0 / (double)g_freq.QuadPart;
+}
