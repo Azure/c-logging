c3d4498493c9ad1614b11aa47592dc39a5294e6b
Dan Cristoloveanu <dcristo@microsoft.com>
2023-06-12 11:19:37 -0700

    Added one test printing all error codes

--- v2/tests/format_message_no_newline_int/format_message_no_newline_int.c
+++ v2/tests/format_message_no_newline_int/format_message_no_newline_int.c
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include <stdio.h>
-#include <stdint.h>
+#include <inttypes.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -32,6 +32,26 @@ static void FormatMessageA_no_newline_fills_995_last_error(void)
     POOR_MANS_ASSERT(strcmp(buffer, "The I/O operation has been aborted because of either a thread exit or an application request.") == 0);
 }
 
+static void FormatMessageA_no_newline_all_error_codes_does_not_crash(void)
+{
+    // arrange
+    char buffer[512];
+
+    for (uint32_t i = 35; i <= 65535; i++)
+    {
+        // act
+        int result = FormatMessageA_no_newline(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, i, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), buffer, sizeof(buffer), NULL);
+
+        // assert
+        // no assert
+        // some of the error codes wil return 0, so we can't really check it
+        if (result > 0)
+        {
+            (void)printf("%" PRIu32 ": %s\r\n", i, buffer);
+        }
+    }
+}
+
 /* very "poor man's" way of testing, as no test harness and mocking framework are available */
 int main(void)
 {
@@ -39,6 +59,7 @@ int main(void)
     _set_abort_behavior(_CALL_REPORTFAULT, _WRITE_ABORT_MSG | _CALL_REPORTFAULT);
 
     FormatMessageA_no_newline_fills_995_last_error();
+    FormatMessageA_no_newline_all_error_codes_does_not_crash();
 
     return 0;
 }
