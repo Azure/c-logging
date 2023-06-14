91f7a1dd260b116b90bf2df3b3cb7e4923fe949e
Dan Cristoloveanu <dcristo@microsoft.com>
2023-06-13 20:21:59 -0700

    Add log_errno_int

---
+++ v2/tests/log_errno_int/log_errno_mocked.c
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include <errno.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+#undef errno
+#define errno (mock_errno())
+
+errno_t mock_errno(void);
+
+#include "log_errno.c"
