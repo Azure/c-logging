df56e763c9db327f7ef53d8e49873ebd03427577
Dan Cristoloveanu <dcristo@microsoft.com>
2023-06-16 10:13:57 -0700

    Address CR comments

--- v2/src/log_errno_linux.c
+++ v2/src/log_errno_linux.c
@@ -11,7 +11,7 @@
 static const char strerror_r_failure_message[] = "failure in strerror_r";
 
 // This array is not used for anything, but rather just to emit a compiler error
-static const char test_message_to_trigger_a_compiler_error[1 * (MESSAGE_BUFFER_SIZE >= sizeof(strerror_r_failure_message))] = { 0 }; /*this construct will generate a compile time error (array of size 0) when LOG_SIZE_REGULAR is not enough to hold even the failure message*/
+static const char test_message_to_trigger_a_compiler_error[1 * (MESSAGE_BUFFER_SIZE >= sizeof(strerror_r_failure_message))] = { 0 }; /*this construct will generate a compile time error (array of size 0) when MESSAGE_BUFFER_SIZE is not enough to hold even the failure message*/
 
 int log_errno_fill_property(void* buffer, int dummy)
 {
--- v2/src/log_errno_win32.c
+++ v2/src/log_errno_win32.c
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include <errno.h>
-#define __STDC_WANT_LIB_EXT1__ 1
 #include <string.h>
 
 #include "macro_utils/macro_utils.h"
@@ -14,7 +13,7 @@
 static const char strerror_s_failure_message[] = "failure in strerror_s";
 
 // This array is not used for anything, but rather just to emit a compiler error
-static const char test_message_to_trigger_a_compiler_error[1 * (MESSAGE_BUFFER_SIZE >= sizeof(strerror_s_failure_message))] = { 0 }; /*this construct will generate a compile time error (array of size 0) when LOG_SIZE_REGULAR is not enough to hold even the failure message*/
+static const char test_message_to_trigger_a_compiler_error[1 * (MESSAGE_BUFFER_SIZE >= sizeof(strerror_s_failure_message))] = { 0 }; /*this construct will generate a compile time error (array of size 0) when MESSAGE_BUFFER_SIZE is not enough to hold even the failure message*/
 
 int log_errno_fill_property(void* buffer, int dummy)
 {
@@ -29,9 +28,6 @@ int log_errno_fill_property(void* buffer, int dummy)
     {
         /* Codes_SRS_LOG_ERRNO_WIN32_01_003: [ Otherwise, log_errno_fill_property shall obtain the errno value. ] */
         /* Codes_SRS_LOG_ERRNO_WIN32_01_004: [ log_errno_fill_property shall call strerror_s with buffer, 512 and the errno value. ] */
-#ifndef WIN32
-#define strerror_s(b, size, errnum) strerror_r(errnum, b, size)
-#endif
         if (strerror_s(buffer, MESSAGE_BUFFER_SIZE, errno) != 0)
         {
             /* Codes_SRS_LOG_ERRNO_WIN32_01_005: [ If strerror_s fails, log_errno_fill_property shall copy in buffer the string failure in strerror_s and return 512. ] */
