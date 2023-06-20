7b2498b1a9c7e6b4ca482f70cb7c5ed499fc007f
Dan Cristoloveanu <dcristo@microsoft.com>
2023-06-20 15:33:52 -0700

    logging_v2

--- v2/inc/c_logging/log_errno.h
+++ v2/inc/c_logging/log_errno.h
@@ -12,11 +12,19 @@
 
 #include "c_logging/log_context.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int log_errno_fill_property(void* buffer, int dummy);
 
 #define LOG_ERRNO() \
     /* Codes_SRS_LOG_ERRNO_WIN32_01_001: [ LOG_ERRNO shall expand to a LOG_CONTEXT_PROPERTY_CUSTOM_FUNCTION with name errno_value, type ascii_char_ptr and value function call being log_errno_fill_property. ] */ \
     /* Codes_SRS_LOG_ERRNO_LINUX_01_001: [ LOG_ERRNO shall expand to a LOG_CONTEXT_PROPERTY_CUSTOM_FUNCTION with name errno_value, type ascii_char_ptr and value function call being log_errno_fill_property. ] */ \
     LOG_CONTEXT_PROPERTY_CUSTOM_FUNCTION(ascii_char_ptr, errno_value, log_errno_fill_property, errno)
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* LOG_ERRNO_H */
