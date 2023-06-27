8504e351dce77be5f13cd9330f7b4936f6b2c3b4
Dan Cristoloveanu <dcristo@microsoft.com>
2023-06-26 23:29:12 -0700

    Fix IWYU

--- v2/tests/logger_abort_ut/logger_abort_ut.c
+++ v2/tests/logger_abort_ut/logger_abort_ut.c
@@ -6,7 +6,6 @@
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <string.h>
 
 #ifdef _MSC_VER
 #include "windows.h"
@@ -15,10 +14,6 @@
 #include "macro_utils/macro_utils.h"
 
 #include "c_logging/log_context_property_type.h"
-#include "c_logging/log_context_property_basic_types.h"
-#include "c_logging/log_context_property_type_ascii_char_ptr.h"
-#include "c_logging/log_context_property_type_if.h"
-#include "c_logging/log_context_property_value_pair.h"
 #include "c_logging/log_level.h"
 #include "c_logging/log_sink_if.h"
 #include "c_logging/log_context.h"
