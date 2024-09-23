9fd46e2fdc24d370e07a1dd4df48d0f96d7d46ae
anporumb <anporumb@users.noreply.github.com>
2024-09-23 09:48:08 -0700

    [manual] update subrepos (#220)
    * propagate anporumb/pchmock
    
    * propagate anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * fixed LNK4217 for log_context_ut
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * update sumodules to lastest master
    
    * revert v2/tests/log_context_ut/log_context_mocked.c to master

--- v2/tests/log_context_ut/log_context_mocked.c
+++ v2/tests/log_context_ut/log_context_mocked.c
@@ -1,6 +1,11 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+#include <stdlib.h> // IWYU pragma: keep
+
+extern void* mock_malloc(size_t size);
+extern void mock_free(void* ptr);
+
 #define malloc mock_malloc
 #define free mock_free
 #define log_internal_error_report mock_log_internal_error_report
