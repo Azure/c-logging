bb85e39a1db684fc0fc1c9f92dbb908b0815c2c9
Andrei Porumb <anporumb@microsoft.com>
2024-09-23 08:59:27 -0700

    fix log_context_mocked.c

--- v2/tests/log_context_ut/log_context_mocked.c
+++ v2/tests/log_context_ut/log_context_mocked.c
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#include <stddef.h>
+#include <stdlib.h> // IWYU pragma: keep
 
 extern void* mock_malloc(size_t size);
 extern void mock_free(void* ptr);
