f105fcd93c4312e3b5b7541c5ece988219bdf81b
Andrei Porumb <anporumb@microsoft.com>
2024-09-23 08:46:41 -0700

    revert v2/tests/log_context_ut/log_context_mocked.c to master

--- v2/tests/log_context_ut/log_context_mocked.c
+++ v2/tests/log_context_ut/log_context_mocked.c
@@ -1,13 +1,6 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <inttypes.h>
-
-extern void* mock_malloc(size_t size);
-extern void mock_free(void* ptr);
-
 #define malloc mock_malloc
 #define free mock_free
 #define log_internal_error_report mock_log_internal_error_report
