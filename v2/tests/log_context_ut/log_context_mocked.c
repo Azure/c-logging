fd231190adbca0405d75b8584d875452804ec8a8
Andrei Porumb <anporumb@microsoft.com>
2024-09-23 08:55:40 -0700

    probably placate iwyu

--- v2/tests/log_context_ut/log_context_mocked.c
+++ v2/tests/log_context_ut/log_context_mocked.c
@@ -1,9 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <inttypes.h>
+#include <stddef.h>
 
 extern void* mock_malloc(size_t size);
 extern void mock_free(void* ptr);
