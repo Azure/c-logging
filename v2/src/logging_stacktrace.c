763e7a52437e9279ba3cdb70d9f8422343e5190c
Dan Cristoloveanu <dcristo@microsoft.com>
2025-08-25 13:28:46 -0700

    Address CR comment

--- v2/src/logging_stacktrace.c
+++ v2/src/logging_stacktrace.c
@@ -24,22 +24,24 @@ void getStackAsString(char* destination, size_t destinationSize)
     /*Codes_SRS_LOGGING_STACKTRACE_01_003: [ If destinationSize is 0, getStackAsString shall return without doing anything. ]*/
     if (destination == NULL || destinationSize == 0)
     {
-        return;
+        /*In theory we could log, but the original implementation only returned, so will leave it at that*/
     }
-
-#ifdef WIN32
-    /*Codes_SRS_LOGGING_STACKTRACE_01_001: [ getStackAsString shall get the stack trace of the current thread and store it in the destination buffer. ]*/
-    /*Codes_SRS_LOGGING_STACKTRACE_01_004: [ getStackAsString shall call get_thread_stack with the current thread ID to get the stack trace. ]*/
-    /*Codes_SRS_LOGGING_STACKTRACE_01_005: [ On Windows, getStackAsString shall use GetCurrentThreadId() to get the current thread ID. ]*/
-    // Use the v2 get_thread_stack functionality to get current thread's stack
-    DWORD currentThreadId = GetCurrentThreadId();
-    get_thread_stack(currentThreadId, destination, destinationSize);
-#else
-    /*Codes_SRS_LOGGING_STACKTRACE_01_006: [ On non-Windows platforms, getStackAsString shall set the destination to an empty string if destinationSize is greater than 0. ]*/
-    // For non-Windows platforms, just return empty string
-    if (destinationSize > 0)
+    else
     {
-        destination[0] = '\0';
-    }
+#ifdef WIN32
+        /*Codes_SRS_LOGGING_STACKTRACE_01_001: [ getStackAsString shall get the stack trace of the current thread and store it in the destination buffer. ]*/
+        /*Codes_SRS_LOGGING_STACKTRACE_01_004: [ getStackAsString shall call get_thread_stack with the current thread ID to get the stack trace. ]*/
+        /*Codes_SRS_LOGGING_STACKTRACE_01_005: [ On Windows, getStackAsString shall use GetCurrentThreadId() to get the current thread ID. ]*/
+        // Use the v2 get_thread_stack functionality to get current thread's stack
+        DWORD currentThreadId = GetCurrentThreadId();
+        get_thread_stack(currentThreadId, destination, destinationSize);
+#else
+        /*Codes_SRS_LOGGING_STACKTRACE_01_006: [ On non-Windows platforms, getStackAsString shall set the destination to an empty string if destinationSize is greater than 0. ]*/
+        // For non-Windows platforms, just return empty string
+        if (destinationSize > 0)
+        {
+            destination[0] = '\0';
+        }
 #endif
+    }
 }
